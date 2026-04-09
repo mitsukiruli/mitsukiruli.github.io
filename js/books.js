3852ce06d9374e9f9a6fc4f6ba4c2593ba413087
mitsukiruli <hgsh010010@gmail.com>
2026-04-09 21:42:38 +0800

    Update books.js

--- js/books.js
+++ js/books.js
@@ -69,7 +69,7 @@ function switchWorld(target, updateHash = true) {
         if (target === '獵人vanilLa✕吸血鬼瑠璃') {
             bannerImg.src = 'img/testimonials/v/無標題306_20251230221312.jpg';
         } else if (target === '鷹院三年級生vanilLa✕鷹院一年級生瑠璃') {
-            bannerImg.src = 'img/testimonials/v/無標題37_20260408222028.jpg';
+            bannerImg.src = 'img/testimonials/v/無標題306_20260409213857.png';
         } else if (target === '樂團') {
             bannerImg.src = 'img/testimonials/v/無標題306_20260331033921.png';
         }
